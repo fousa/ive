5b86139a0c04fde6d097ce0adba1312e5d7aaf43
Jelle Vandebeeck <jelle@fousa.be>
2014-04-30 10:42:50 +0200

    Better configuration handling.

--- lib/ive/base.rb
+++ lib/ive/base.rb
@@ -11,7 +11,7 @@ module Ive
       end
 
       def bump(type, git=false)
-        if config = Ive::Xcode.new.config
+        fetch_config do |config|
           if git
             if Git.changes?
               puts "-- This repository has uncommited changes please commit these changes before performing a version bump."
@@ -22,15 +22,24 @@ module Ive
           else
             Bump.public_send(type, config)
           end
-        else
-          puts "-- No project file found"
         end
       end
 
       def version
-        if config = Ive::Xcode.new.config
+        fetch_config do |config|
           puts "-- Version #{config.info_plist.marketing_version}"
           puts "-- Build version #{config.info_plist.version}"
+        end
+      end
+
+      private
+
+      def fetch_config &block
+        xcode = Ive::Xcode.new
+        if config = xcode.config
+          yield config if block_given?
+        elsif xcode
+          puts "-- .ive configuration file missing or invalid."
         else
           puts "-- No project file found"
         end
